0d5f0cf34aa4d33a056562cde78a79ea26a6f725
Jirphat Saetae <jirphat.aok@gmail.com>
2023-08-16 21:37:10 +0700

    605

--- Homework/HW06/hw06-5-prime-number.cpp
+++ Homework/HW06/hw06-5-prime-number.cpp
@@ -28,7 +28,6 @@ int main() {
         if (num >= 2) {
             for ( int i = 2; i * i <= num; i++ ) {
                 if (num % i == 0) {
-                    printf("gg%d\n",i) ;
                     is_prime = 0;
                     break;
                 }
